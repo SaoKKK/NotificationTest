13cb71f9b679b7398d741b8d723256a127999eec
Saori Kohno <saorikohno@yahoo.com>
2016-02-26 16:09:10 +0900

    Initial Commit

---
+++ NotificationTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  NotificationTest
+//
+//  Created by 河野 さおり on 2016/02/26.
+//  Copyright © 2016年 河野 さおり. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
